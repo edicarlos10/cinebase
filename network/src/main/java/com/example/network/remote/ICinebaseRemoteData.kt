4a5f0b9812d3858a734646a4a6cc482061a03840
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-23 12:48:59 -0300

    :art: renamed interfaces

--- network/src/main/java/com/example/network/remote/ICinebaseRemoteData.kt
+++
@@ -1,4 +0,0 @@
-package com.example.network.remote
-
-interface ICinebaseRemoteData {
-}
--- network/src/main/java/com/example/network/remote/api/ICineApiClient.kt
+++ network/src/main/java/com/example/network/remote/api/ICineApiClient.kt
@@ -1,15 +1,15 @@
 package com.example.network.remote.api
 
-import com.example.domain.cinebase.model.NowPlaying
+import com.example.network.remote.response.NowPlayingResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface ICinebaseApiClient {
+interface ICineApiClient {
 
     @GET("now_playing")
     fun getNowPlaying(
-        @Query("api_key") apiKey: String,
+        @Query("api_key") apiKey: String = "b19f46767a72e4812181f01a5d9085b8",
         @Query("page") page: Int = 1,
         @Query("language") language: String = "pt-br",
-        ): List<NowPlaying>
+        ): List<NowPlayingResponse>
 }
