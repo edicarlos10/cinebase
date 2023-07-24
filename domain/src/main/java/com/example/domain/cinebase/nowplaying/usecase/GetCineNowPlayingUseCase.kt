e3a163137f09da098bc5229658ba9c288aa4c016
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-24 11:18:37 -0300

    :art: added use case to now playing

---
+++ domain/src/main/java/com/example/domain/cinebase/nowplaying/usecase/GetCineNowPlayingUseCase.kt
@@ -0,0 +1,16 @@
+package com.example.domain.cinebase.nowplaying.usecase
+
+import com.example.domain.cinebase.ICineRepository
+import com.example.domain.cinebase.nowplaying.model.NowPlaying
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetCineNowPlayingUseCase @Inject constructor(private val cineRepository: ICineRepository) {
+    suspend fun execute(page: Int?, language: String?): Flow<List<NowPlaying>> {
+        return getNowPlaying(page = page, language = language)
+    }
+
+    private suspend fun getNowPlaying(page: Int?, language: String?): Flow<List<NowPlaying>> {
+        return cineRepository.getNowPlaying(page = page, language = language)
+    }
+}
