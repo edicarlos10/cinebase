73f766fd1ccb5ffd5934a46fcaf98d4ea00e91d6
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-27 11:57:34 -0300

    :art: added top bar

---
+++ app/src/main/java/com/example/cinebase/features/home/TopBar.kt
@@ -0,0 +1,50 @@
+package com.example.cinebase.features.home
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    title: String= "",
+    onBackPressed: (() -> Unit)? = null,
+    containerColor: Color = MaterialTheme.colorScheme.surface,
+    actions: @Composable RowScope.() -> Unit = {}
+) {
+    Surface(shadowElevation = 2.dp) {
+        TopAppBar(
+            title = { Text(text = title, maxLines = 1, overflow = TextOverflow.Ellipsis) },
+            navigationIcon = {
+                if (onBackPressed != null) {
+                    TopBarIcon(onClick = { onBackPressed() }, imageVector = Icons.Default.ArrowBack)
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = containerColor,
+                actionIconContentColor = contentColorFor(containerColor),
+                navigationIconContentColor = contentColorFor(containerColor),
+                titleContentColor = contentColorFor(containerColor)
+            ),
+            actions = actions,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTopBar() {
+    TopBar()
+}
