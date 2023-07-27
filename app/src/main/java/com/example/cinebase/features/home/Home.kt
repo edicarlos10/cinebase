176b50fe443589a1e559dccb4bf8ab5a216a41c9
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-27 11:58:11 -0300

    :art: adjusting screens

--- app/src/main/java/com/example/cinebase/features/home/Home.kt
+++ app/src/main/java/com/example/cinebase/features/home/Home.kt
@@ -3,10 +3,18 @@ package com.example.cinebase.features.home
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.cinebase.features.home.nowplaying.NowPlaying
 import com.example.cinebase.ui.theme.CinebaseTheme
@@ -14,8 +22,24 @@ import com.example.domain.cinebase.base.State
 
 @Composable
 fun Home(homeViewModel: HomeViewModel?) {
-    homeViewModel?.getNowPlaying()
-    Scaffold { paddingValues ->
+    var isSearching by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        homeViewModel?.getNowPlaying()
+    }
+    Scaffold(
+        topBar = {
+            TopBar(
+                title = "Filmes",
+                actions = {
+                    TopBarIcon(
+                        onClick = { isSearching = true },
+                        imageVector = Icons.Default.Search
+                    )
+                }
+            )
+        }
+    ) { paddingValues ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -28,7 +52,9 @@ fun Home(homeViewModel: HomeViewModel?) {
                     }
 
                     is State.Data -> {
-                        NowPlaying(response.data)
+                        NowPlaying(response = response.data, Modifier.padding(16.dp), onClick = {
+                            //TODO: open detail
+                        })
                     }
 
                     is State.Loading -> {
--- app/src/main/java/com/example/cinebase/features/home/nowplaying/NowPlaying.kt
+++ app/src/main/java/com/example/cinebase/features/home/nowplaying/NowPlaying.kt
@@ -1,15 +1,19 @@
 package com.example.cinebase.features.home.nowplaying
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -19,26 +23,39 @@ import com.example.cinebase.features.home.Constants
 import com.example.domain.cinebase.home.model.NowPlaying
 
 @Composable
-fun NowPlaying(response: NowPlaying) {
-    Text("Em alta", modifier = Modifier.padding(16.dp))
+fun NowPlaying(response: NowPlaying, modifier: Modifier, onClick: () -> Unit = {}) {
+    Row {
+        Text(
+            "Em alta",
+            modifier = Modifier.padding(start = 16.dp, top = 8.dp),
+            style = MaterialTheme.typography.headlineSmall.copy(
+                fontWeight = FontWeight.Light
+            )
+        )
+    }
     LazyRow(
         modifier = Modifier
             .padding(top = 48.dp)
     ) {
-        items(response.results!!) { item ->
-            Card(
-                modifier = Modifier
-                    .padding(start = 16.dp, bottom = 16.dp)
-            ) {
-                AsyncImage(
-                    model = ImageRequest.Builder(context = LocalContext.current)
-                        .data(Constants.imageUrlBase + item?.poster_path)
-                        .crossfade(true).build(),
-                    error = painterResource(R.drawable.ic_broken_image),
-                    placeholder = painterResource(R.drawable.loading_animation),
-                    contentDescription = "imagem do filme",
-                    contentScale = ContentScale.FillBounds
-                )
+        response.results?.let { listResult ->
+            items(listResult) { item ->
+                Card(
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .clickable {
+                            onClick()
+                        }
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(context = LocalContext.current)
+                            .data(Constants.imageUrlBase + item?.poster_path)
+                            .crossfade(true).build(),
+                        error = painterResource(R.drawable.ic_broken_image),
+                        placeholder = painterResource(R.drawable.loading_animation),
+                        contentDescription = "imagem do filme",
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
             }
         }
     }
