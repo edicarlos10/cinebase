ba2c4faa60531426e97ecef94df76148990790bb
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-28 14:43:14 -0300

    :art: added fun search in network module

--- network/src/main/java/com/example/network/CineRepository.kt
+++ network/src/main/java/com/example/network/CineRepository.kt
@@ -2,6 +2,7 @@ package com.example.network
 
 import com.example.domain.cinebase.ICineRepository
 import com.example.domain.cinebase.home.model.NowPlaying
+import com.example.domain.cinebase.home.model.Search
 import com.example.network.remote.ICineRemoteData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -11,5 +12,13 @@ import javax.inject.Inject
 class CineRepository @Inject constructor(private val cineRemoteData: ICineRemoteData) : ICineRepository {
     override suspend fun getNowPlaying(page: Int?, language: String?): Flow<NowPlaying> {
         return cineRemoteData.getNowPlaying(page = page,language = language).flowOn(Dispatchers.IO)
+    }
+
+    override suspend fun getMovieSearch(
+        query: String,
+        page: Int?,
+        language: String?
+    ): Flow<Search> {
+        return  cineRemoteData.getMovieSearch(query = query, page = page, language = language).flowOn(Dispatchers.IO)
     }
 }
--- network/src/main/java/com/example/network/remote/CineRemoteData.kt
+++ network/src/main/java/com/example/network/remote/CineRemoteData.kt
@@ -1,6 +1,7 @@
 package com.example.network.remote
 
 import com.example.domain.cinebase.home.model.NowPlaying
+import com.example.domain.cinebase.home.model.Search
 import com.example.network.remote.api.ICineApiClient
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -9,5 +10,13 @@ import javax.inject.Inject
 class CineRemoteData @Inject constructor(private val apiICineApiClient: ICineApiClient) : ICineRemoteData {
     override suspend fun getNowPlaying(page: Int?, language: String?): Flow<NowPlaying> = flow {
         emit(apiICineApiClient.getNowPlaying(page = page,language = language).toNowPlaying())
+    }
+
+    override suspend fun getMovieSearch(
+        query: String,
+        page: Int?,
+        language: String?
+    ): Flow<Search> = flow {
+        emit(apiICineApiClient.getMovieSearch(query = query,page = page, language = language).toSearch())
     }
 }
