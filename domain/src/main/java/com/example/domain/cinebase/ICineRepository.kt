e00bef48618c2aaddc06c3748f45eb2a3cf3b86c
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-28 14:35:45 -0300

    :art: added fun to interface

--- domain/src/main/java/com/example/domain/cinebase/ICineRepository.kt
+++ domain/src/main/java/com/example/domain/cinebase/ICineRepository.kt
@@ -1,8 +1,10 @@
 package com.example.domain.cinebase
 
 import com.example.domain.cinebase.home.model.NowPlaying
+import com.example.domain.cinebase.home.model.Search
 import kotlinx.coroutines.flow.Flow
 
 interface ICineRepository {
     suspend fun getNowPlaying(page: Int?, language: String?): Flow<NowPlaying>
+    suspend fun getMovieSearch(query: String, page: Int?, language: String?): Flow<Search>
 }
--- network/src/main/java/com/example/network/remote/ICineRemoteData.kt
+++ network/src/main/java/com/example/network/remote/ICineRemoteData.kt
@@ -1,8 +1,10 @@
 package com.example.network.remote
 
 import com.example.domain.cinebase.home.model.NowPlaying
+import com.example.domain.cinebase.home.model.Search
 import kotlinx.coroutines.flow.Flow
 
 interface ICineRemoteData {
     suspend fun getNowPlaying (page: Int?, language: String?): Flow<NowPlaying>
+    suspend fun getMovieSearch (query: String, page: Int?, language: String?): Flow<Search>
 }
