d162e4cadf47733b28b10670c17000208b6b39fb
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-28 16:43:34 -0300

    :art: created search fun in view model

--- app/src/main/java/com/example/cinebase/features/home/HomeViewModel.kt
+++ app/src/main/java/com/example/cinebase/features/home/HomeViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.cinebase.base.State
 import com.example.domain.cinebase.home.model.NowPlaying
+import com.example.domain.cinebase.home.model.Search
+import com.example.domain.cinebase.home.usecase.GetCineMovieSearchUseCase
 import com.example.domain.cinebase.home.usecase.GetCineNowPlayingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,7 +17,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getCineNowPlayingUseCase: GetCineNowPlayingUseCase
+    private val getCineNowPlayingUseCase: GetCineNowPlayingUseCase,
+    private val getCineMovieSearchUseCase: GetCineMovieSearchUseCase
 ): ViewModel() {
 
     private  val _nowPlayingState = MutableStateFlow<State<NowPlaying>>(State.loading())
@@ -25,6 +28,9 @@ class HomeViewModel @Inject constructor(
 //    init {
 //        getNowPlaying()
 //    }
+
+    private  val _searchState = MutableStateFlow<State<Search>>(State.loading())
+    val search = _searchState.asStateFlow()
 
     fun getNowPlaying(page: Int? = 1, language: String? = "pt-br"){
         viewModelScope.launch {
@@ -36,5 +42,21 @@ class HomeViewModel @Inject constructor(
                 _nowPlayingState.emit(State.Data(it))
             }
         }
+    }
+
+    fun getMovieSearch(query: String, page: Int? = 1, language: String? = "pt-br"){
+        viewModelScope.launch {
+            getCineMovieSearchUseCase.execute(query, page, language).onStart {
+                _searchState.emit(State.Loading)
+            }.catch {
+                _searchState.emit(State.Error(cause = it))
+            }.collect{
+                _searchState.emit(State.Data(it))
+            }
+        }
+    }
+
+    suspend fun setStateToLoading(){
+        _nowPlayingState.emit(State.Idle)
     }
 }
