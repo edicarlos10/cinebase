97862d2428bedc32329248f12917a1540c9433a5
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-31 09:20:38 -0300

    :art: added fun to remote data and repository

--- network/src/main/java/com/example/network/CineRepository.kt
+++ network/src/main/java/com/example/network/CineRepository.kt
@@ -3,6 +3,7 @@ package com.example.network
 import com.example.domain.cinebase.ICineRepository
 import com.example.domain.cinebase.home.model.NowPlaying
 import com.example.domain.cinebase.home.model.Search
+import com.example.domain.cinebase.home.model.Upcoming
 import com.example.network.remote.ICineRemoteData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -12,6 +13,10 @@ import javax.inject.Inject
 class CineRepository @Inject constructor(private val cineRemoteData: ICineRemoteData) : ICineRepository {
     override suspend fun getNowPlaying(page: Int?, language: String?): Flow<NowPlaying> {
         return cineRemoteData.getNowPlaying(page = page,language = language).flowOn(Dispatchers.IO)
+    }
+
+    override suspend fun getUpcoming(page: Int?, language: String?): Flow<Upcoming> {
+        return cineRemoteData.getUpcoming(page = page,language = language).flowOn(Dispatchers.IO)
     }
 
     override suspend fun getMovieSearch(
--- network/src/main/java/com/example/network/remote/CineRemoteData.kt
+++ network/src/main/java/com/example/network/remote/CineRemoteData.kt
@@ -2,6 +2,7 @@ package com.example.network.remote
 
 import com.example.domain.cinebase.home.model.NowPlaying
 import com.example.domain.cinebase.home.model.Search
+import com.example.domain.cinebase.home.model.Upcoming
 import com.example.network.remote.api.ICineApiClient
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -10,6 +11,10 @@ import javax.inject.Inject
 class CineRemoteData @Inject constructor(private val apiICineApiClient: ICineApiClient) : ICineRemoteData {
     override suspend fun getNowPlaying(page: Int?, language: String?): Flow<NowPlaying> = flow {
         emit(apiICineApiClient.getNowPlaying(page = page,language = language).toNowPlaying())
+    }
+
+    override suspend fun getUpcoming(page: Int?, language: String?): Flow<Upcoming> = flow {
+        emit(apiICineApiClient.getUpcoming(page = page,language = language).toUpcoming())
     }
 
     override suspend fun getMovieSearch(
