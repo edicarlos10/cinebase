5cc9b3bbf2c8ab9f3dd57d0729a92ed885a6776f
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-31 10:15:42 -0300

    :art: improved layout

--- app/src/main/java/com/example/cinebase/features/home/Home.kt
+++ app/src/main/java/com/example/cinebase/features/home/Home.kt
@@ -1,5 +1,7 @@
 package com.example.cinebase.features.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -7,13 +9,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cinebase.features.home.base.BottomNavigation
 import com.example.cinebase.features.home.base.SearchTopBar
 import com.example.cinebase.features.home.enuns.NavScreens.Home
 import com.example.cinebase.features.home.enuns.NavScreens.values
 import com.example.cinebase.features.home.nowplaying.NowPlayingBody
 import com.example.cinebase.features.home.search.SearchBody
+import com.example.cinebase.features.home.upcoming.UpComingBody
 import com.example.cinebase.ui.theme.CinebaseTheme
 
 @Composable
@@ -24,6 +29,7 @@ fun Home(homeViewModel: HomeViewModel?) {
 
     LaunchedEffect(Unit) {
         homeViewModel?.getNowPlaying()
+        homeViewModel?.getUpcoming()
     }
     Scaffold(
         topBar = {
@@ -35,16 +41,24 @@ fun Home(homeViewModel: HomeViewModel?) {
                     currentScreen = screen
                     if (currentScreen.compareTo(Home) == 0) {
                         homeViewModel?.getNowPlaying()
+                        homeViewModel?.getUpcoming()
                     }
                 },
                 isSelected = { screen ->
                     currentScreen == screen
                 })
+        },
+        content = { paddingValues ->
+            Column(
+            ) {
+                NowPlayingBody(homeViewModel = homeViewModel, paddingValues = paddingValues)
+            }
+            Column (modifier = Modifier.padding(top = 280.dp)) {
+                UpComingBody(homeViewModel = homeViewModel, paddingValues = paddingValues)
+            }
+            SearchBody(homeViewModel = homeViewModel, paddingValues = paddingValues)
         }
-    ) { paddingValues ->
-        NowPlayingBody(homeViewModel = homeViewModel, paddingValues = paddingValues)
-        SearchBody(homeViewModel = homeViewModel, paddingValues = paddingValues)
-    }
+    )
 }
 
 @Preview(showBackground = true)
